98f2bd88647a3410b68d5f2ae6755a1059bd72b3
Anirudha Gupta <35772421+anirugu@users.noreply.github.com>
2023-01-15 10:56:12 +0530

    Add project files.

---
+++ IMDBSearch.cs
@@ -0,0 +1,40 @@
+﻿using AngleSharp.Browser;
+using AngleSharp.Html.Dom;
+using AngleSharp.Html.Parser;
+
+namespace IMDB
+{
+    public class IMDBSearch
+    {
+        private readonly IHtmlDocument _document;
+
+        public IMDBSearch(string html)
+        {
+            var parser = new HtmlParser();
+            _document = parser.ParseDocument(html);
+        }
+
+        public IEnumerable<string> GetShortCodes()
+        {
+            var anchors = _document.QuerySelectorAll(".lister-item-header a[href]").Select(x => x.Attributes["href"].Value.Split("/")[2]);
+            return anchors;
+        }
+
+        public IEnumerable<string> GetTVCodes()
+        {
+            return _document.QuerySelectorAll(".lister-item-year.text-muted.unbold").Where(x => x.TextContent.IndexOf("–") > 0)
+                   .Select(
+                       elem =>
+                       {
+                           if (elem.NextElementSibling == null)
+                           {
+                               return elem.PreviousElementSibling.Attributes["href"].Value.Split("/")[4];
+                           }
+                           else
+                           {
+                               return elem.PreviousElementSibling.Attributes["href"].Value.Split("/")[4];
+                           }
+                       });
+        }
+    }
+}
